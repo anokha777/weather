8ca97cf185044e079ce68fcea7a378d82d977f35
Brajesh Anokha <anokha777@gmail.com>
2020-10-20 13:35:03 +0530

    weather app

---
+++ precache-manifest.94b5c259e073f9c78e076c7dc4f0a297.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61324cb0dc20915f91fd357d46f7c81c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "911b589748448226fbc6",
+    "url": "/static/css/2.16d46856.chunk.css"
+  },
+  {
+    "revision": "6a4531f0f8dd80f87e65",
+    "url": "/static/css/main.a8d13503.chunk.css"
+  },
+  {
+    "revision": "911b589748448226fbc6",
+    "url": "/static/js/2.5aef3859.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.5aef3859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4531f0f8dd80f87e65",
+    "url": "/static/js/main.9c33b682.chunk.js"
+  },
+  {
+    "revision": "d079d425fda0b6b0e384",
+    "url": "/static/js/runtime-main.62e09ec8.js"
+  }
+]);
